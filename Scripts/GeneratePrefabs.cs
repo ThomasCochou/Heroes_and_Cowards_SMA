994c10c3b4a0f5f60c8ef8568ef206fd40d19dde
a8larivi <a8larivi@enib.fr>
2022-01-13 15:34:58 +0100

    axel

--- Scripts/GeneratePrefabs.cs
+++ Scripts/GeneratePrefabs.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 public class GeneratePrefabs : MonoBehaviour 
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
@@ -10,57 +11,89 @@ public class GeneratePrefabs : MonoBehaviour
     public bool free = false;
     public float agentSpeed = 100.0f;
     public float simuSpeed = 1.0f;
+	public bool start;
+	public bool isPaused = true;
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
-    	
-    	Vector3 position3D;
-
-    	int i = 0;
-
-        for (; i < heroesNumber; i++)
-		{
-			var randomValueX = Random.Range(0.0f, 50.0f);
-			var randomValueY = Random.Range(0.0f, 50.0f);
-			var randomValueZ = Random.Range(0.0f, 50.0f);
-
-			if(threeDim)
-			{
-				position3D =  new Vector3(randomValueX,randomValueY, randomValueZ);
-			}
-			else
-			{
-				position3D =  new Vector3(randomValueX, 0.5f, randomValueZ);
-			}
-			GameObject clone = (GameObject)Instantiate(heroe,position3D, Quaternion.identity);
-			clone.GetComponent<AgentBehavior>().id = i;
-		}
-
-        for (; i < heroesNumber + cowardsNumber; i++)
-		{
-
-			var randomValueX = Random.Range(0.0f, 50.0f);
-			var randomValueY = Random.Range(0.0f, 50.0f);
-			var randomValueZ = Random.Range(0.0f, 50.0f);
-
-			if(threeDim)
-			{
-				position3D =  new Vector3(randomValueX,randomValueY, randomValueZ);
-			}
-			else
-			{
-				position3D =  new Vector3(randomValueX, 0.5f, randomValueZ);
-			}
-			GameObject clone = (GameObject)Instantiate(coward, position3D, Quaternion.identity);
-			clone.GetComponent<AgentBehavior>().id = i;
-		}
-        
+	
     }
+
+	void OnGUI () 
+	{
+		if(isPaused == true)
+		{
+			// string nbH;
+			GUI.contentColor = Color.black;
+
+			GUI.Box(new Rect(Screen.width / 2 - 120 , Screen.height / 2 - 40, 250, 150), "", new GUIStyle {normal = new GUIStyleState {background = Texture2D.whiteTexture } } );
+
+			GUI.Label(new Rect(Screen.width / 2 - 80, Screen.height / 2 - 20, 200, 20), "Cowards number = " );
+			GUI.Label(new Rect(Screen.width / 2 + 40, Screen.height / 2 - 20, 200, 20), cowardsNumber.ToString());
+			cowardsNumber = (int)GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 , 200, 20),cowardsNumber, 0, 100);
+
+			GUI.Label(new Rect(Screen.width / 2 - 80, Screen.height / 2 + 20, 200, 20), "Heroes number = " );
+			GUI.Label(new Rect(Screen.width / 2 + 40, Screen.height / 2 + 20, 200, 20), heroesNumber.ToString());
+			heroesNumber = (int)GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 40, 200, 20),heroesNumber, 0, 100);
+
+			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 60, 80, 40), "Continue"))
+			{
+				start = true;
+			}
+		}
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if(start == true)
+		{
+			isPaused = false;
+
+			Vector3 position3D;
+
+			int i = 0;
+
+			for (; i < heroesNumber; i++)
+			{
+				var randomValueX = Random.Range(0.0f, 50.0f);
+				var randomValueY = Random.Range(0.0f, 50.0f);
+				var randomValueZ = Random.Range(0.0f, 50.0f);
+
+				if(threeDim)
+				{
+					position3D =  new Vector3(randomValueX,randomValueY, randomValueZ);
+				}
+				else
+				{
+					position3D =  new Vector3(randomValueX, 0.5f, randomValueZ);
+				}
+				GameObject clone = (GameObject)Instantiate(heroe,position3D, Quaternion.identity);
+				clone.GetComponent<AgentBehavior>().id = i;
+			}
+
+			for (; i < heroesNumber + cowardsNumber; i++)
+			{
+
+				var randomValueX = Random.Range(0.0f, 50.0f);
+				var randomValueY = Random.Range(0.0f, 50.0f);
+				var randomValueZ = Random.Range(0.0f, 50.0f);
+
+				if(threeDim)
+				{
+					position3D =  new Vector3(randomValueX,randomValueY, randomValueZ);
+				}
+				else
+				{
+					position3D =  new Vector3(randomValueX, 0.5f, randomValueZ);
+				}
+				GameObject clone = (GameObject)Instantiate(coward, position3D, Quaternion.identity);
+				clone.GetComponent<AgentBehavior>().id = i;
+			}  
+
+			start = false; 
+		}
 
 	}
 }
--- Scripts/PauseMenu.cs
+++ Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class PauseMenu : MonoBehaviour
 	private bool toggleFree;
 	private float agentSliderSpeed;
 	private float simuSliderSpeed;
+	public float numberOfCowards;
+	public float numberOfHeroes;
 
 	// Start is called before the first frame update
 	void Start()
@@ -18,6 +21,8 @@ public class PauseMenu : MonoBehaviour
 		toggleFree = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().free;
 		agentSliderSpeed = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().agentSpeed;
 		simuSliderSpeed = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().simuSpeed;
+		numberOfCowards = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().cowardsNumber;
+		numberOfHeroes = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().heroesNumber;
 	}
 
 	void OnGUI () 
@@ -25,31 +30,37 @@ public class PauseMenu : MonoBehaviour
 		if(isPaused)
 		{
 			GUI.contentColor = Color.black;
-			GUI.Box(new Rect(Screen.width / 2 - 125 , Screen.height / 2 - 150, 250, 200), "", new GUIStyle {normal = new GUIStyleState { background = Texture2D.whiteTexture } } );
+			GUI.Box(new Rect(Screen.width / 2 - 125 , Screen.height / 2 - 150, 250, 250), "", new GUIStyle {normal = new GUIStyleState { background = Texture2D.whiteTexture } } );
 
 			toggleThreeDim = GUI.Toggle(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 140, 200, 20),toggleThreeDim, "	3D");
 
 			toggleFree = GUI.Toggle(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 120, 200, 20),toggleFree, "	Free");
 			
 			
-			GUI.Label(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 100, 200, 20), "Agent speed = " );
+			GUI.Label(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 100, 200, 20), "Agent speed = " );
 			GUI.Label(new Rect(Screen.width / 2 + 40, Screen.height / 2 - 100, 200, 20), ((int)agentSliderSpeed).ToString() );
 			agentSliderSpeed = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 80, 200, 20),agentSliderSpeed, 1.0f, 5000.0f);
 
-			GUI.Label(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 60, 200, 20), "Simu speed = " );
+			GUI.Label(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 60, 200, 20), "Simu speed = " );
 			GUI.Label(new Rect(Screen.width / 2 + 40, Screen.height / 2 - 60, 200, 20), (simuSliderSpeed).ToString("0.##") );
 			simuSliderSpeed = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 40, 200, 20),simuSliderSpeed, 0.0f, 5.0f);
-
-			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2, 80, 40), "Continue"))
-			{
-				isPaused = false;
-			}
-
 
 			GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().threeDim = toggleThreeDim;
 			GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().free = toggleFree;
 			GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().agentSpeed = agentSliderSpeed;
 			GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GeneratePrefabs>().simuSpeed = simuSliderSpeed;
+
+			if(GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 40, 80, 40), "Continue"))
+			{
+				isPaused = false;
+			}
+
+			if(GUI.Button(new Rect(Screen.width / 2 + 20, Screen.height / 2 + 40, 80, 40), "Restart"))
+			{
+				SceneManager.LoadScene("MainScene");
+			}
+
+			
 
 		}
 	}
